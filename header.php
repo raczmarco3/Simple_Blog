b004837236783682c6c53d90c02cfcad9be195b4
Rácz Marco <raczmarco3@gmail.com>
2022-07-22 12:50:44 +0200

    login and logout moved to header.php

--- header.php
+++ header.php
@@ -1,5 +1,50 @@
+<?php
+    include "backend/user.php";
+    session_start();
+?>
+
 <!DOCTYPE html>
     <head>
         <title>Simple Blog</title>
     </head>
     <body>
+        <div class="right">
+            <?php
+                if(!isset($_SESSION["username"])) {
+            ?>
+                    <form method="POST" action="">
+                        Username: <input type="text" name="username" required>
+                        Password: <input type="password" name="password" required>
+                        <input type="submit" name="login" value="Login">
+                    </form>
+            <?php
+                } else {
+                    echo "Welcome back ", $_SESSION["username"];
+            ?>
+                    <form method="POST" action="">
+                        <input type="submit" name="logout" value="Logout">
+                    </form>
+            <?php
+                    if(isset($_POST["logout"])) {
+                        unset($_SESSION["username"]);
+                        header('Refresh: 0;');
+                    }
+                }
+            ?>
+        </div>
+        <div class="left">
+
+<?php
+    if(isset($_POST["login"])) {
+        if(empty($_POST["username"]) || empty($_POST["password"])) {
+            echo "Input field must not be empty!";
+        } else {
+            $user = new User();
+            if($user -> logIn($_POST["username"], md5($_POST["password"]))) {
+                header('Refresh: 0;');
+            } else {
+                echo "Wrong credentials!";
+            }
+        }
+    }
+?>
--- index.php
+++ index.php
@@ -1,44 +1,8 @@
 <?php
     include "header.php";
-    include "backend/user.php";
-    session_start();
 ?>
-    <?php
-        if(!isset($_SESSION["username"])) {
-    ?>
-            <form method="POST" action="">
-                Username: <input type="text" name="username" required>
-                Password: <input type="password" name="password" required>
-                <input type="submit" name="login" value="Login">
-            </form>
-    <?php
-        } else {
-            echo "Welcome back ", $_SESSION["username"];
-    ?>
-            <form method="POST" action="">
-                <input type="submit" name="logout" value="Logout">
-            </form>
-    <?php
-            if(isset($_POST["logout"])) {
-                unset($_SESSION["username"]);
-                header('Refresh: 0;');
-            }
-        }
-    ?>
+    
 
 <?php
-    if(isset($_POST["login"])) {
-        if(empty($_POST["username"]) || empty($_POST["password"])) {
-            echo "Input field must not be empty!";
-        } else {
-            $user = new User();
-            if($user -> logIn($_POST["username"], md5($_POST["password"]))) {
-                header('Refresh: 0;');
-            } else {
-                echo "Wrong credentials!";
-            }
-        }
-    }
-
     include "footer.php";
 ?>
